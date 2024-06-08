52a5b6e3872147290ec7eb5c1176421c88e74e44
Jerry Jin <jinbeige@berkeley.edu>
2024-06-08 11:54:25 +0800

    latest from local

--- Modeling/CMR_IA/fitting/object_funcs.py
+++ Modeling/CMR_IA/fitting/object_funcs.py
@@ -316,48 +316,35 @@ def obj_func_3(param_vec, df_study, df_test, sem_mat, sources):
     df_laggp = df_simu.groupby(['type','lag']).s_resp.mean().to_frame(name='yes_rate').reset_index()
 
     # get d prime
-    df_dprime = pd.DataFrame()
-    df_dprime['lag'] = [2,4,6,8,16]
-    df_dprime['I_z_hr'] = sp.stats.norm.ppf(df_laggp.loc[df_laggp.type == 1, 'yes_rate'].astype(float))
-    df_dprime['I_z_far'] = np.mean(sp.stats.norm.ppf(df_laggp.loc[df_laggp.type == 0, 'yes_rate'].astype(float)))
-    df_dprime['I_dprime'] = df_dprime['I_z_hr'] - df_dprime['I_z_far']
-    df_dprime['A_z_hr'] = sp.stats.norm.ppf(df_laggp.loc[df_laggp.type == 2, 'yes_rate'].astype(float))
-    df_dprime['A_z_far'] = sp.stats.norm.ppf(df_laggp.loc[df_laggp.type == 3, 'yes_rate'].astype(float))
-    df_dprime['A_dprime'] = df_dprime['A_z_hr'] - df_dprime['A_z_far']
+    # df_dprime = pd.DataFrame()
+    # df_dprime['lag'] = [2,4,6,8,16]
+    # df_dprime['I_z_hr'] = sp.stats.norm.ppf(df_laggp.loc[df_laggp.type == 1, 'yes_rate'].astype(float))
+    # df_dprime['I_z_far'] = np.mean(sp.stats.norm.ppf(df_laggp.loc[df_laggp.type == 0, 'yes_rate'].astype(float)))
+    # df_dprime['I_dprime'] = df_dprime['I_z_hr'] - df_dprime['I_z_far']
+    # df_dprime['A_z_hr'] = sp.stats.norm.ppf(df_laggp.loc[df_laggp.type == 2, 'yes_rate'].astype(float))
+    # df_dprime['A_z_far'] = sp.stats.norm.ppf(df_laggp.loc[df_laggp.type == 3, 'yes_rate'].astype(float))
+    # df_dprime['A_dprime'] = df_dprime['A_z_hr'] - df_dprime['A_z_far']
 
     # get the vectors
-    I_hr = df_laggp.loc[df_laggp.type == 1, "yes_rate"].to_numpy()
-    A_hr = df_laggp.loc[df_laggp.type == 2, "yes_rate"].to_numpy()
-    I_dprime = df_dprime['I_dprime'].to_numpy()
-    A_dprime = df_dprime['A_dprime'].to_numpy()
-    # cut off I_dprime and A_dprime above 5 or below -5 to avoid inf
-    I_dprime = np.clip(I_dprime, -5, 5)
-    A_dprime = np.clip(A_dprime, -5, 5)
-    I_dprime[np.isnan(I_dprime)] = 10
-    A_dprime[np.isnan(A_dprime)] = 10
+    I_hr = df_laggp.loc[df_laggp.type == 'single_old', "yes_rate"].to_numpy()
+    I_far = np.mean(df_laggp.loc[df_laggp.type == 'single_new', 'yes_rate'].astype(float))
+    A_hr = df_laggp.loc[df_laggp.type == 'pair_old', "yes_rate"].to_numpy()
+    A_far = df_laggp.loc[df_laggp.type == 'pair_new', "yes_rate"].to_numpy()
 
     # ground truth
     I_hr_gt = np.array([0.865, 0.811, 0.752, 0.746, 0.708])
+    I_far_gt = 0.12
     A_hr_gt = np.array([0.843, 0.787, 0.720, 0.735, 0.646])
-    I_dprime_gt = np.array([2.31, 2.05, 1.85, 1.84, 1.68])
-    A_dprime_gt = np.array([1.36, 1.29, 1.37, 1.58, 1.39])
+    A_far_gt = np.array([0.406, 0.371, 0.285, 0.259, 0.202])
 
     # calculate the error
-    err = np.mean(np.power(I_hr - I_hr_gt,2)) * 10 + np.mean(np.power(A_hr - A_hr_gt,2)) * 10 \
-        + np.mean(np.power(I_dprime - I_dprime_gt,2)) + np.mean(np.power(A_dprime - A_dprime_gt,2))
-
-    # apply some constraints
-    if not (I_dprime[0] > I_dprime[1] and I_dprime[1] > I_dprime[2] and I_dprime[2] > I_dprime[3] and I_dprime[3] > I_dprime[4]):
-        err += 1
-    if not (I_hr[0] > I_hr[1] and I_hr[1] > I_hr[2] and I_hr[2] > I_hr[3] and I_hr[3] > I_hr[4]):
-        err += 1
-    if not (I_hr > A_hr).all():
-        err += 1 
+    err = np.mean(np.power(I_hr - I_hr_gt, 2)) + np.mean(np.power(A_hr - A_hr_gt, 2)) \
+        + np.power(I_far - I_far_gt, 2) * 5 + np.mean(np.power(A_far - A_far_gt, 2))
     
     cmr_stats = {}
     cmr_stats['err'] = err
     cmr_stats['params'] = param_vec
-    cmr_stats['stats'] = [I_hr, A_hr, I_dprime, A_dprime]
+    cmr_stats['stats'] = [I_hr, I_far, A_hr, A_far]
 
     return err, cmr_stats
 
